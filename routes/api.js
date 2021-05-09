5b0016b2c4caa338d3785b5452685ab040079f46
VanJackal <v@njackal.com>
2021-05-08 21:50:39 -0500

    added route outline

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/articles', (req, res, next) => { // Gets the list of articles for the feed with shortened info
+	
+});
 
+router.get('/articles/:id', (req, res, next) => { // Gets full info of the article with the given id
+
+});
+
+router.post('/articles', (req, res, next) => { // Fetches new articles and adds them to the database
+
+});
+
+router.put('/articles/:id', (req, res, next) => { // sets the readstate of the article with the given id
+
+});
 
 module.exports = router;

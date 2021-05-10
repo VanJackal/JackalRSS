2c82165181fd32aa4fd4093ebb9495398d07942e
VanJackal <v@njackal.com>
2021-05-10 10:12:01 -0500

    basic rss fetch for testing

--- rss.js
+++ rss.js
@@ -1,4 +1,15 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
 
-const feedURL = 'https://www.relay.fm/cortex/feed'
+async function fetchFeed(url){
+	let feed = await parser.parseURL(url);
+	console.log(feed.title);
+
+	feed.items.forEach(item => {
+		console.log(item.title + " : " + item.link);
+	});
+}
+
+module.exports = {
+	fetchFeed: fetchFeed
+};

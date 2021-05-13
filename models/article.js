b28214546d430438edcf8e8ab1b5d7e36dc320fe
VanJackal <v@njackal.com>
2021-05-13 16:50:57 -0500

    added parameters to article and feed models

--- models/article.js
+++ models/article.js
@@ -2,10 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-    title:{
+	feedid:{
+		type: String,
+		required: true
+	}
+	title:{
         type: String,
-        required: [true, 'Title of the article is required']
-    }
+        required: true
+    },
+	description:{
+		type: String,
+		required: true
+	},
+	pubDate:{
+		type: Date,
+		default: Date.now
+	},
+	link:{
+		type: String
+	},
+	enclosure:{
+		type: String
+	},
+	content:{
+		type: String
+	}
 });
 
 const Article = mongoose.model('article', ArticleSchema);
--- models/feed.js
+++ models/feed.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FeedSchema = new Schema({
-
+	feedid:{
+		type: String,
+		required: true
+	},
+	title:{
+		type: String,
+		required: true
+	},
+	description:{
+		type: String,
+		required: true
+	},
+	link:{
+		type: String,
+		required: true
+	}
 });
 
 const Feed = mongoose.model('article', FeedSchema);

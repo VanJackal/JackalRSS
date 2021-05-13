110e54e33a7b675d8726ac11f2f4f9ec29a2484b
TheJKL <vanjackal@protonmail.com>
2021-05-13 07:56:28 -0500

    seperated get articles into a get and post route

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const rss = require('../rss');
 const Article = require('../models/article');
 
-router.get('/articles', async (req, res, next) => { // Gets(including fetching new articles) the list of articles for the feed with shortened info
-    await rss.fetchFeed(req.body.url);
-    Article.find({}, 'title').then(data => res.json(data))
+router.get('/articles', (req, res, next) => { // Gets(including fetching new articles) the list of articles for the feed with shortened info
+    Article.find({}, 'title').then(data => res.json(data));
+});
+
+router.post('/articles', (req, res, next) => {
+    //console.log(req.body.url);
+    rss.fetchFeed(req.body.url).then(data => res.json(data));
 });
 
 router.get('/articles/:id', (req, res, next) => { // Gets full info of the article with the given id
--- rss.js
+++ rss.js
@@ -5,12 +5,12 @@ const Article = require('./models/article');
 async function fetchFeed(url){
 	let feed = await parser.parseURL(url);
 	console.log(feed.title);
-	await feed.items.forEach(async item => await addItem(item))
-}
-
-async function addItem(item){
-	await console.log(item.title + " : " + item.link);
-	return Article.create({title:item.title})
+	numArticles = 0;
+	feed.items.forEach(item => {
+		Article.create({title:item.title});
+		numArticles++;
+	});
+	return {new:numArticles};
 }
 
 module.exports = {

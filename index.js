0995ed283036a141b385ffab09ed483f1aef55ef
VanJackal <v@njackal.com>
2021-07-31 12:01:00 -0500

    removed uses of deprecated funtinos

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB, {useNewUrlParser: true})
+mongoose.connect(process.env.DB, {useNewUrlParser: true,useFindAndModify: false,useUnifiedTopology: true})
     .then(() => console.log('Database Connected.'))
     .catch(err => console.log(err));
+
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(express.json());
 
 app.use('/api', routes);
 

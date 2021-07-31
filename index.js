b864745794927e2e34035b045829b2cd9b566f50
VanJackal <v@njackal.com>
2021-07-31 11:34:35 -0500

    remove deprecated bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const routes = require('./routes/api')
 require('dotenv').config();
 
@@ -12,15 +11,11 @@ mongoose.connect(process.env.DB, {useNewUrlParser: true})
     .then(() => console.log('Database Connected.'))
     .catch(err => console.log(err));
 
-//mongoose.Promise = global.Promise;
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-app.use(bodyParser.json());
 
 app.use('/api', routes);
 

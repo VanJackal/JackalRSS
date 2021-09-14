302b494ca4e8ca25109f24a213b3e24c96f682ad
VanJackal <v@njackal.com>
2021-09-14 12:12:38 -0500

    added remove feed function

--- routes/api.js
+++ routes/api.js
@@ -34,4 +34,9 @@ router.get('/feeds/:feedid/articles', (req, res, next) => { // Gets the list of 
     Article.find({feedid:req.params.feedid}, {title:1, pubDate:1,read:1}).then(data => res.json(data));
 });
 
+router.delete('/feeds/:feedid', (req, res, next) => {//Remove feed and its connected articles
+	Article.remove({feedid:req.params.feedid});
+	Feed.deleteMany({_id:req.params.feedid}).then(data => res.json(data));
+})
+
 module.exports = router;

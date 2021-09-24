75d8e2070ce3fe7dcf10a2756400aecbe90a6058
VanJackal <v@njackal.com>
2021-09-24 08:19:50 -0500

    CORS aaaaaah

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ mongoose.connect(process.env.DB, {useNewUrlParser: true,useFindAndModify: false,
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials","true")
     next();
 });
 

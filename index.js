153011436b881665dddf616f3dc80b7d2ab77674
jackal <v@njackal.com>
2022-06-14 09:50:06 -0500

    moved db initialization into jrss-db package

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB, {useNewUrlParser: true,useFindAndModify: false,useUnifiedTopology: true})
-    .then(() => console.log('Database Connected.'))
-    .catch(err => console.log(err));
-
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
--- lib/jrss-db/index.ts
+++ lib/jrss-db/index.ts
@@ -2,6 +2,15 @@ import {Article, IArticle} from './article'
 import {Feed, IFeed} from './feed'
 import {Folder, IFolder} from './folder'
 import {User, IUser} from './user'
+import * as mongoose from "mongoose";
+
+function init():void {
+    mongoose.connect(process.env.DB, {useNewUrlParser: true,useFindAndModify: false,useUnifiedTopology: true})
+        .then(() => console.log('Database Connected.'))
+        .catch(err => console.log(err));
+}
+
+init()
 
 export {
     Article,

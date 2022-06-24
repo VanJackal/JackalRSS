883d935fcedb2ec0513a703476d4b018c8f75578
VanJackal <v@njackal.com>
2022-06-24 12:27:05 -0500

    get /folders implementation

--- lib/routes/folders.ts
+++ lib/routes/folders.ts
@@ -1,10 +1,32 @@
-import {Folder} from "jrss-db";
+import {Folder, IFolder} from "jrss-db";
 import * as lib from './foldersLib'
 import express = require('express')
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
+//add auth to all routes and subroutes of /folders
+router.all("/(.*)", (req, res, next) => {
+    if (!req.user) return res.sendStatus(401);
+    next();
+})
+
+router.get("/", async (req,res) => {
+    let folders:IFolder[] = await lib.getFolders(req.user._id);
+    req.json(folders);
+    req.status(200);
+})
+
+router.post("/", (req,res) => {
+
+})
+
+router.get("/:folderid", (req,res) => {
+
+})
+router.patch("/:folderid", (req,res) => {
+
+})
+router.delete("/:folderid", (req,res) => {
 
 })
 

5ab3a09d88f76520b2609b3cd5b9504188813472
jackal <v@njackal.com>
2022-06-30 12:04:50 -0500

    removed refactored route

--- lib/routes/util.ts
+++ lib/routes/util.ts
@@ -1,5 +1,4 @@
 import express = require('express')
-import {Feed} from "../jrss-db";
 import rss = require('rss-handler')
 import {logger} from 'logging'
 
@@ -22,29 +21,6 @@ router.post('/feeds/info', async (req, res) => { //gets the info from an rss fee
 router.post('/feeds/refresh', (req, res) =>{
     if (!req.user) return res.sendStatus(401);
     rss.refreshAll(req.user._id).then((data) => res.json(data));
-})
-
-router.get('/feeds/folders',async (req, res) => {//TODO this needs to be moved to a folders.ts route file
-    if (!req.user) return res.sendStatus(401);
-    logger.error('GET /util/feeds/folders -> route not properly implemented')
-    const foldersObj = await Feed.aggregate([
-        {
-            "$match": { userid: req.user.id }
-        },
-        {
-            "$group": {
-                _id: "$folder"
-            }
-        }
-    ]);
-
-    const folders = foldersObj.map(folder => {
-        return folder._id;
-    })
-
-    res.json(folders.filter(folder=>{
-        return folder ? folder != false : false;//if the folder isnt null or an empty string
-    }))
 })
 
 export {

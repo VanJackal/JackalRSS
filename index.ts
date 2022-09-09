d3c90511e94ef6b08d21f86df4c454d6ed97d242
VanJackal <v@njackal.com>
2022-09-09 11:19:03 -0500

    Folders router added to index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express = require('express');
 import session = require('express-session');
 import passport = require('passport');
-import {ArticlesRouter, FeedsRouter, UtilRouter} from 'routes'
+import {ArticlesRouter, FeedsRouter, FoldersRouter, UtilRouter} from 'routes'
 import userRoutes = require('routes/users');
 import {logger} from 'logging'
 import 'jrss-db'//initializes mongoose
@@ -35,6 +35,7 @@ app.use('*',(req,res,next) => {
 
 app.use('/api/articles', ArticlesRouter);
 app.use('/api/feeds', FeedsRouter);
+app.use('/api/folders', FoldersRouter);
 app.use('/api/util', UtilRouter);
 app.use('/api/users', userRoutes);
 
